c940bff8a62325649404dd5ee86a22e773ed2372
Rodrick Lankford <lankfordr@carleton.edu>
2023-10-14 16:35:59 -0500

    Fixes to projects and CSS

--- src/Home.js
+++ src/Home.js
@@ -11,30 +11,37 @@ function Home() {
     <div className='home'>
         <div className='home_row1'>
           <div className='home_about'>
-            <h1>
-              About Me
-            </h1>
-            <p>
-              <p>Name: Rodrick V. Lankford</p>
-              <p>School: Carleton College</p>
-              <p>Age: 22 years</p>
-              <p>From: Dallas, Tx</p>
-              <p>Hello, my name is Rodrick Victdeon Lankford. I am a skilled
-               software developer who enjoys the challenge of solving complex 
-               software development and design problems. I graduated from 
-               Carleton College in June 2023 with a bachelor's degree in Computer Science. 
-               My peers know me as reliable and someone who correctly completes assigned 
-               duties earlier rather than later. One of my biggest career motivators 
-               involves the implementation of innovative and creative software that push 
-               the capabilities of technology to a new level. That said, I desire to 
-               continue applying my skills to web development, software 
-               development/engineering, and game development to improve the 
-               quality of life for all.</p>
-            </p>
-            <p>Fun fact my favorite color is gold.</p>
-            <form method="get" action="Rodrick Victdeon Lankford Resume.pdf">
-              <button type="submit" className='button'>Download Resume!</button>
-            </form>
+            <div className='image_container'>
+              <img src="/Me.jpg" alt="image" className='home_image'></img>
+              <div className='home_about'>
+
+                
+                <h1>
+                  About Me
+                </h1>
+                <p>
+                  <p>Name: Rodrick V. Lankford</p>
+                  <p>School: Carleton College</p>
+                  <p>Age: 22 years</p>
+                  <p>From: Dallas, Tx</p>
+                  <p>Hello World, my name is Rodrick Victdeon Lankford. I am a skilled
+                  programmer who enjoys the challenge of solving challenging problems. I graduated from 
+                  Carleton College in June 2023 with a bachelor's degree in Computer Science. 
+                  My peers know me as reliable and someone who completes assigned 
+                  duties promptly. One of my biggest career motivators 
+                  involves the implementation of innovative and creative software that push 
+                  the capabilities of technology to a new level. That said, I desire to 
+                  continue applying my skills to improve the 
+                  quality of life for all.</p>
+                </p>
+                <p>Fun fact my favorite color is gold.</p>
+                <form method="get" action="Resume For Rodrick Lankford.pdf">
+                  <button type="submit" className='button'>Download Resume!</button>
+                </form>
+              </div>
+            </div>
+            
+            
           </div>
         </div>
 
@@ -42,40 +49,60 @@ function Home() {
           {/* Experience */}
           <div className='home_r2elements'>
             <h1>
-              Experience:
+              Technical Experiences:
             </h1>
             <ul>
-              <li><h4>SayKid SDE Internship:</h4> During this internship I worked with 
-              a team to develop a game for which SayKid's toy robot could interactively play against
-              a user. Also I worked with a partner to develop SayKid's webpage futher.</li>
-              <li><h4>SayKid SDE Externship:</h4> This was where I spent my winter break developing several
-               games for SayKid's toy robot.</li>
-              <li><h4>Vex Robotics:</h4> At this competition I worked with a large team to
+              <li><h3>SayKid SDE Internship:</h3> Assist with software testing and debugging.
+              Collaborate with team members to design and implement an interactive game module that implements a greedy algorithm using JavaScript for Saykid’s toy robot.
+              Participate in code reviews to ensure high-quality code.
+              Research and learn new technologies such as DynamoDB, AWS, PHP, and WordPress to help develop a dynamic full-stack web page.
+              Troubleshoot and resolve software issues.
+              Document software specifications and user manuals.
+              Assist in the deployment of software.
+              </li>
+              <li><h3>SayKid Game Developer Externship:</h3> Code and debug game features using Natural Language Processing and JavaScript.
+              Create and maintain documentation for software development processes and procedures.
+              Test and iterate game mechanics to ensure a smooth and engaging user experience.
+              Use API to handle search queries to give the robot more intuitive response choices.
+              Collaborate with coworkers to integrate audio assets into software.
+              Participate in team meetings and provide input on strategies.
+              Assist with bug tracking and troubleshooting during the game development process.
+              Contribute to the overall creative and technical vision of the game.
+              </li>
+              <li><h3>Vex Robotics:</h3> At this competition I worked with a large team to
               develop the code and design for a rover like robot. I held a leadership role which required me to manage 
               each individual group and facilitate communication between them.</li>
-              <li><h4>BEST Robotics:</h4> This competition was much like Vex Robotics except with the caveat that my team was
-               required to build all of the robot parts from scratch and needed to meet specifications. My main responsibilities 
-               where to manage the coding group and ensure deadlines where met promptly.</li>
+              <li><h3>BEST Robotics:</h3> Led the coding group who worked on developing the code for a rover-type robot who was designed to throw and pick up tokens and tennis balls to score points.
+              Worked with the marketing team whose job was to market off our robot to a panel of judges using detailed design schematics and an overview of our code.
+              </li>
+              <li><h3>The Avenue:</h3> Built a website using React designed to allow clients to enroll in The Avenues Talent Search program.
+              Collaborate with client to understand requirements.
+              Test and debug code to ensure functionality
+              Apply up-to-date industry trends and technologies to solve problems.
+              Made processing clients information and applications more efficient reducing the time and manpower needed for the company.
+              Work independently to complete projects.
+              Applied technical knowledge to offer advice on automating overly complex business processes, and improved their application process.
+              Document code and processes for future reference.
+              </li>
             </ul>
           </div>
           {/* Image */}
-          <div className='home_r2elements'>
-            <img src="/Me.jpg" alt="image" className='home_image'></img>
-
-          </div>
+          
           {/* Skills */}
           <div className='home_r2elements'>
             <h1>
               Skills:
             </h1>
             <ol>
-              <li>Python3</li>
+              <li>Python</li>
               <li>Java</li>
               <li>JavaScript</li>
               <li>HTML</li>
               <li>CSS</li>
-              <li>React.js</li>
+              <li>React.JS</li>
+              <li>SQL</li>
               <li>Socket.io</li>
+              <li>.Net</li>
               <li>C#</li>
               <li>C++</li>
             </ol>
--- src/Projects.js
+++ src/Projects.js
@@ -12,9 +12,14 @@ function Projects() {
                 <div className='project_title'>
                     Pictaphone
                     <div className='description'>
-                        This project is a game developed as a website in which players can 
-                        choose to play on a pc or remotely on a mobile device. Much like the game pictonary
+                        This project is a website game that players can 
+                        play on a PC or remotely on a mobile device. Much like the game Pictionary,
                         users are given a prompt and asked to draw said prompt to their best ability.
+                    </div>
+
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford, Jeremy Beckler, Will Flemming, James Berger, Brian Rhee</p>
                     </div>
                 </div>
             </p>
@@ -40,9 +45,14 @@ function Projects() {
                 <div className='project_title'>
                     Amazon Replica
                     <div className='description'>
-                        I built this website mainly to develop my skills with creating websites. I used Amazon's
-                        old website as inpiration, and replicated as much as I could to the best 
-                        of my ability at the time.
+                        I built this website using Amazon's website as inspiration to hone my web development skills.
+                        This website features user authentication and sign-in, allowing users to save the items in
+                        their basket.
+                    </div>
+                    
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford</p>
                     </div>
                 </div>
             </p>
@@ -54,10 +64,70 @@ function Projects() {
                     JavaScript
                 </div>
                 <div className='language'>
-                    React.js
+                    React.JS
                 </div>
                 <div className='language'>
                     Firebase
+                </div>
+            </div>
+            
+        </a>
+
+        <a href="https://github.com/Rodrick12123/Artificial-Ecology/tree/main" target="_blank" className='projects_indv'>
+            <img src="/eco.PNG" alt="image" />
+            <p>
+                
+                <div className='project_title'>
+                    Artificial Ecology
+                    <div className='description'>
+                    This program generates a evolving ecosytem that represents how cannibalistic 
+                    organisms tend to gain a natural advantage in a limited ecosystem.
+                    </div>
+                    
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford, Anya Vostinar</p>
+                    </div>
+                </div>
+            </p>
+            <div className='languages'>
+                <div className='language'>
+                    C++
+                </div>
+                <div className='language'>
+                    Shell
+                </div>
+                <div className='language'>
+                    HTML
+                </div>
+                <div className='language'>
+                    Empirical Library
+                </div>
+            </div>
+            
+        </a>
+
+        <a href="https://github.com/Rodrick12123/Computer-Graphics" target="_blank" className='projects_indv'>
+            <img src="/raster.PNG" alt="image" />
+            <p>
+                
+                <div className='project_title'>
+                    Computer Graphic Models
+                    <div className='description'>
+                    Project includes several c files that work together to do computer graphic-related tasks, 
+                    including but not limited to imageing with shadows and lighting, rasterization, 3D/2D views, 
+                    and camera positioning.
+                    </div>
+                    
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford, Josh Davis, Jerimiah Mensah</p>
+                    </div>
+                </div>
+            </p>
+            <div className='languages'>
+                <div className='language'>
+                    C programming
                 </div>
             </div>
             
@@ -69,44 +139,95 @@ function Projects() {
                 <div className='project_title'>
                     World Cup Data
                     <div className='description'>
-                        Another website designed to allow its users to filter specific data and stats 
-                        to get a better understanding of factors that played big roles in targeted World Cup Statistics.
+                        A website designed to allow its users to filter specific data and stats 
+                        using MySQL and REST API to understand the factors that played big roles in World Cup Statistics.
+                    </div>
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford, Thea Traw</p>
                     </div>
                 </div>
+                
             </p>
             <div className='languages'>
                 <div className='language'>
                     HTML
                 </div>
                 <div className='language'>
-                    Python3
+                    Python
                 </div>
                 <div className='language'>
                     SQL
+                </div>
+                <div className='language'>
+                    JavaScript
                 </div>
             </div>
             
         </a>
 
-        <a href='https://observablehq.com/d/a12e08e997d1f2aa' target="_blank" className='projects_indv'>
-            <img src="/Base.PNG" alt="image" />
+        <a href="https://github.com/Rodrick12123/Artificial-Extinction-Model" target="_blank" className='projects_indv'>
+            <img src="/extinct.PNG" alt="image" />
             <p>
-               
+                
                 <div className='project_title'>
-                    Baseball Evolution
+                    Artificial Extinction Model
                     <div className='description'>
-                        Encodes MLB data from all years up to 2023 as differn't visualizations that
-                        allows users to get a better idea of how the league and individual teams have 
-                        changed over the years.
+                    Simulates a simple ecosytem model where each species is encoded 
+                    with a different color and can move around within a grid/ecosystem. All species
+                    can reproduce, die, and go extinct. However, some organisms mutate, which eventually
+                    leads to their evolution.
+                    </div>
+                    
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford, Sam Lengyel, Kai Weiner</p>
                     </div>
                 </div>
             </p>
             <div className='languages'>
                 <div className='language'>
+                    C++
+                </div>
+                <div className='language'>
+                    Shell
+                </div>
+                <div className='language'>
                     HTML
                 </div>
                 <div className='language'>
-                    Plot
+                    Empirical Library
+                </div>
+            </div>
+            
+        </a>
+
+        <a href='https://avenue-application.web.app/' target="_blank" className='projects_indv'>
+            <img src="/Talent search.PNG" alt="image" />
+            <p>
+               
+                <div className='project_title'>
+                    The Avenue Application
+                    <div className='description'>
+                        Site that allows The Avenue clients to apply to the talent search
+                        program. It also sends a uniquely formated verison of the applicant's info to the company.
+                    </div>
+                    
+                    <div className='project_contributors'>
+                        <strong>Contributors: </strong>
+                        <p>Rodrick Lankford</p>
+                    </div>
+                </div>
+            </p>
+            <div className='languages'>
+                <div className='language'>
+                    Email.JS
+                </div>
+                <div className='language'>
+                    HTML
+                </div>
+                <div className='language'>
+                    React
                 </div>
                 <div className='language'>
                     JavaScript
@@ -134,7 +255,7 @@ function Projects() {
         </a>
 
       </div>
-      <header>More projects from GitHub to be added here</header>
+      <header>I intend on improving project listings and adding more projects from github, and my local directiores at a later date.</header>
     </div>
   )
 }
